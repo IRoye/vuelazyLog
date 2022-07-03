2ae828ebf4d99593edcbaf777932da51dd412599
yuyi <yuyi@shindata.com>
2022-07-03 19:14:02 +0800

    chore(项目): 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Description: demo main page
+ * @Author: yuyi
+ * @Date: 2022-07-03 16:56:26
+ * @LastEditors: yuyi
+ * @LastEditTime: 2022-07-03 19:08:53
+-->
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
